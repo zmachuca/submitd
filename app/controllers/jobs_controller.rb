e9bb849d976b05bb98f8d3dd818603e5322ad8be
Zach Machuca <zachmachuca@gmail.com>
2019-06-30 15:14:33 -0400

    changed order

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,7 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :show, :index]
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :show, :index]
 
   def index
     @jobs = Job.all
